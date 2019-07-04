e19ca88cacf7d556152541004886c02ab67b3cb4
khoapn-1940 <pham.nguyen.khoa@sun-asterisk.com>
2019-07-04 11:31:51 +0700

    Fix

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -3,4 +3,22 @@ class AvatarUploader < CarrierWave::Uploader::Base
   def store_dir
     "app/assets/avatar"
   end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+
+  def size_range
+    1..10.megabytes
+  end
+
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
+  protected
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+  end
 end
